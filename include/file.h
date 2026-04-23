b4e57380f514dcaed0b66af62aad2e6b7eb76141
tracehillonsign <tracehillonsign@gmail.com>
2026-04-23 21:14:21 +0300

    feat: функции проверки является ли путь файлом/директорией

---
+++ include/file.h
@@ -0,0 +1,14 @@
+/*
+	revis/include/filep.h
+
+	author: tracehillonsign
+*/
+
+#pragma once
+#ifndef FILE_H
+#define FILE_H
+
+int ifile(const char *path);
+int idir(const char *path);
+
+#endif
---
+++ src/file.c
@@ -0,0 +1,41 @@
+/*
+        revis/src/file.c
+
+        author: tracehillonsign
+*/
+
+#include "common.h"
+
+/* Проверяет является ли путь файлом. */
+int ifile(const char *path)
+{
+    struct stat st;
+
+    if (stat(path, &st) == -1) {
+        perror("ошибка stat(ifile : file)");
+        return -1;
+    }
+
+    if (S_ISREG(st.st_mode)) {
+        return 0;
+    } else {
+        return -2;
+    }
+}
+
+/* Проверяет является ли путь директорией. */
+int idir(const char *path)
+{
+    struct stat st;
+
+    if (stat(path, &st) == -1) {
+        perror("ошибка stat(idir : file)");
+        return -1;
+    }
+
+    if (S_ISDIR(st.st_mode)) {
+        return 0;
+    } else {
+        return -2;
+    }
+}
